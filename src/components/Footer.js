1fc20e4a8c53f8eebcb79f5b6557d63e725f658a
Monish Lal <monishlal0521@gmail.com>
2024-08-30 20:26:57 +0530

    preFinal commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="w-full flex items-center justify-center h-[10vh] bg-slate-200 shadow-lg font-bold mb-0">
       <p className="text-center  cursor-pointer">
         &copy; 2024 <span className="text-orange-500">hellofood</span> ||
-        Developed by &nbsp;<span className="text-orange-500">Monish Lal</span>
+        Developed by <span className="text-orange-500">Monish Lal</span>
       </p>
     </div>
   );
