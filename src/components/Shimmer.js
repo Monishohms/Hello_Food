4286d0c0f427e29d935e7d050a7a390f357810fe
Monish Lal <monishlal0521@gmail.com>
2024-08-30 19:28:33 +0530

    preFinal Commit

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,7 +1,7 @@
 const Shimmer = () => {
   return (
     <div>
-      {/* <div className="p-3 m-2  max-w-96 rounded-xl bg-slate-300 flex flex-wrap justify-normal">
+      <div className="p-3 m-2  max-w-96 rounded-xl bg-slate-300 flex flex-wrap justify-normal">
         <div className="bg-slate-300">
           <div className=" rounded-xl h-44 w-60 bg-slate-200"> </div>
         </div>
@@ -10,7 +10,7 @@ const Shimmer = () => {
         <p className=" w-60 bg-slate-200"></p>
 
         <p className=" w-60 bg-slate-200"> </p>
-      </div> */}
+      </div>
     </div>
   );
 };
